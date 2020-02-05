17fd82b421375c74370231511011587bbdf8e085
Steve Cohn <smcohn@gmail.com>
2020-02-04 19:50:50 -0700

    Fixed provider.

--- accounts/dev/provider.tf
+++ accounts/dev/provider.tf
@@ -4,8 +4,8 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
   region                  = "us-west-2"
-  shared_credentials_file = "/home/smcohn/.aws/credentials.cohns"
-  profile                 = "cohns"
+  shared_credentials_file = "/Users/smcohn/.aws/credentials"
+  profile                 = "default"
   assume_role {
     role_arn     = "arn:aws:iam::451089431772:role/cohns-dev-admin-role"
     session_name = "cohns-dev-admin-session"
